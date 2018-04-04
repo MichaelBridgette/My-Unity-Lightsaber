f3e49b6093476e62113b4b194c5940cf513a91c6
michaelbridgette <c00205948@itcarlow.ie>
2018-04-04 23:07:12 +0100

    Build complete

---
+++ Lightsaber/Assets/Lightsaber.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightsaber : MonoBehaviour {
+
+
+    LineRenderer lineRend;
+    public Transform startPos;
+    public Transform endPos;
+
+    private float textureOffset = 0.0f;
+    bool on = true;
+    private Vector3 endPosExtendedPos;
+
+	// Use this for initialization
+	void Start () {
+        lineRend = GetComponent<LineRenderer>();
+        endPosExtendedPos = endPos.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(on)
+            {
+                on = false;
+            }
+            else
+            {
+                on = true;
+
+            }
+        }
+
+        if(on)
+        {
+            endPos.localPosition = Vector3.Lerp(endPos.localPosition, endPosExtendedPos, Time.deltaTime * 5f);
+        }
+        else
+        {
+            endPos.localPosition = Vector3.Lerp(endPos.localPosition, startPos.localPosition, Time.deltaTime * 3f);
+
+        }
+
+        lineRend.SetPosition(0, startPos.position);
+        lineRend.SetPosition(1, endPos.position);
+
+
+        textureOffset -= Time.deltaTime * 2f;
+
+        if(textureOffset < -10.0f)
+        {
+            textureOffset += 10.0f;
+        }
+        lineRend.sharedMaterials[1].SetTextureOffset("_MainTex", new Vector2(textureOffset, 0));
+    }
+}
---
+++ Lightsaber/Assets/LightsaberSound.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsaberSound : MonoBehaviour {
+
+
+    public AudioSource audioSource;
+    public AudioClip on;
+    public AudioClip off;
+    public AudioClip hum;
+    bool isOn = true;
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(isOn)
+            {
+                audioSource.loop = false;
+                isOn = false;
+                audioSource.Stop();
+                audioSource.clip = off;
+                audioSource.Play();
+                Debug.Log("OFF");
+            }
+            else
+            {
+                audioSource.loop = false;
+                isOn = true;
+                audioSource.Stop();
+                audioSource.clip = on;
+                audioSource.Play();
+                Debug.Log("ON");
+            }
+        }
+        if(isOn && !audioSource.isPlaying)
+        {
+            audioSource.clip = hum;
+            audioSource.Play();
+            audioSource.loop = true;
+        }
+
+	}
+}
